2ab73959658158014c0806958f3b7c0e0bcf4311
ProfissaoRodrigo <163425279+ProfissaoRodrigo@users.noreply.github.com>
2024-03-18 22:22:50 -0300

    Add files via upload

---
+++ # Projeto Números dos andares do Hotel.py
@@ -0,0 +1,26 @@
+# Projeto Números dos andares do Hotel
+# Imprimir todos os númenos exceto o 13 (usando for . . . in range)
+
+for i in a range(1,20):
+    if(i == 13):
+        continue
+    else:
+        print(i)
+
+# Imprimir todos os númenos exceto o 13 (usando while)
+
+while (contador <= 20):
+    if(contador == 13):
+        contador = contador + 1
+        continue
+    else:
+        print(contador)
+        contador = contador + 1
+
+# Imprimir todos os númenos exceto o 13 em ordem decrescente
+
+for i in a range(20,0,-1)
+    if(1 == 13):
+        continue
+    else:
+        print(i)
